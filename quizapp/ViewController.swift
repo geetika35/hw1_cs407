89c6dd09cc191978a1700fa4575166dbc40c846b
Geetika Batra <geetikabatra@geetikas-macbook-pro.local>
2016-02-15 22:36:04 -0600

    Initial Commit

---
+++ quizapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quizapp
+//
+//  Created by Geetika Batra on 2/15/16.
+//  Copyright © 2016 Geetika Batra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
